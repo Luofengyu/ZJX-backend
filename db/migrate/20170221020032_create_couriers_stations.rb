e400b4b877fad35bb1bce00155d4224ad0e394c9
d <1213704928@qq.com>
2017-04-22 09:36:23 +0800

    修改了数据库增加关系model

--- db/migrate/20170221020032_create_couriers_stations.rb
+++
@@ -1,8 +0,0 @@
-class CreateCouriersStations < ActiveRecord::Migration[5.0]
-  def change
-    create_table :couriers_stations do |t|
-      t.references :courier
-      t.references :station
-    end
-  end
-end
--- db/migrate/20170221055331_create_factories_stations.rb
+++
@@ -1,8 +0,0 @@
-class CreateFactoriesStations < ActiveRecord::Migration[5.0]
-  def change
-    create_table :factories_stations do |t|
-      t.references :factory
-      t.references :station
-    end
-  end
-end
